2e29d4534ee99f6579a5870c239210da47a2801d
Ltfall <Ltfall@qq.com>
2022-05-10 13:51:01 +0800

    ...

--- src/views/NewTask.vue
+++ src/views/NewTask.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>hello, world!</div>
+  <a-card>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <add-spider />
+        </el-col>
+        <el-col :span="12">
+          <img src="../assets/img/spider.png" alt="" />
+        </el-col>
+      </el-row>
+  </a-card>
 </template>
 
 <script>
-export default {
+import AddSpider from "../components/AddSpider.vue";
 
+export default {
+  name: "SpiderAdd",
+  components: {
+    AddSpider,
+  },
+  setup() {}
 }
 </script>
 
 <style>
-
 </style>
