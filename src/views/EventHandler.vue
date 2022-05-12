e7262d14cfb50b53b93ab5e815c4d15eb3849c34
Ltfall <Ltfall@qq.com>
2022-05-12 09:31:01 +0800

    一些小更改

--- src/views/EventHandler.vue
+++ src/views/EventHandler.vue
@@ -1,13 +1,85 @@
 <template>
-  <div>hello, world</div>
+  <a-table
+    :columns="columns"
+    :row-key="record => record.login.uuid"
+    :data-source="data"
+    :pagination="pagination"
+    :loading="loading"
+    @change="handleTableChange"
+  >
+    <template slot="name" slot-scope="name"> {{ name.first }} {{ name.last }} </template>
+  </a-table>
 </template>
-
 <script>
+import axios from 'axios';
+
+const queryData = params => {
+  return axios.get('http://localhost:5000/apiRequestSender/query/listJobs');
+};
+const columns = [
+  {
+    title: '探测时间',
+    dataIndex: 'create_time',
+    // sorter: true,
+    width: '20%',
+    // scopedSlots: { customRender: 'name' },
+  },
+  {
+    title: '',
+    dataIndex: 'gender',
+    // filters: [
+    //   { text: 'Male', value: 'male' },
+    //   { text: 'Female', value: 'female' },
+    // ],
+    width: '20%',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+];
+
 export default {
-
-}
+  data() {
+    return {
+      data: [],
+      pagination: {},
+      loading: false,
+      columns,
+    };
+  },
+  mounted() {
+    this.fetch();
+  },
+  methods: {
+    handleTableChange(pagination, filters, sorter) {
+      console.log(pagination);
+      const pager = { ...this.pagination };
+      pager.current = pagination.current;
+      this.pagination = pager;
+      this.fetch({
+        results: pagination.pageSize,
+        page: pagination.current,
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...filters,
+      });
+    },
+    fetch(params = {}) {
+      this.loading = true;
+      queryData({
+        results: 10,
+        ...params,
+      }).then(({ data }) => {
+        const pagination = { ...this.pagination };
+        // Read total count from server
+        // pagination.total = data.totalCount;
+        pagination.total = 200;
+        this.loading = false;
+        this.data = data.results;
+        this.pagination = pagination;
+      });
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
