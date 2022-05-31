1ed0864cb0153db2f36e5159c0cd52288f4e8e32
howahh <871759032@qq.com>
2022-05-31 15:30:24 +0800

    hh

--- src/views/WebsiteTrace.vue
+++ src/views/WebsiteTrace.vue
@@ -61,11 +61,6 @@
                 }}
               </a-timeline-item>
               <a-timeline-item>
-                <a-icon
-                  slot="dot"
-                  type="clock-circle-o"
-                  style="font-size: 16px"
-                />
                 新增网站:{{ this.class1838[1].url }}<br />{{
                   this.class1838[1].date
                 }}
@@ -106,10 +101,7 @@
               </a-timeline-item>
               <a-timeline-item>
                 <a-icon
-                  slot="dot"
-                  type="clock-circle-o"
-                  style="font-size: 16px"
-                />
+
                 新增网站:{{ this.class870[1].url }}<br />{{
                   this.class1838[1].date
                 }}
